13d9b616a140880414e6f52c01075a4700e24e97
Andrew <andrewjnieto@gmail.com>
2019-04-26 11:06:18 -0400

    Update JewelsAndStonesSolutions.java

--- StringProblems/JewelsAndStonesSolutions.java
+++ StringProblems/JewelsAndStonesSolutions.java
@@ -1,5 +1,38 @@
-package random;
+import java.util.HashSet;
+import java.util.Set;
 
 public class JewelsAndStonesSolutions {
-
+	public static int numJewelsInStones(String J, String S) {
+		if (J.length() == 0 || S.length() == 0) {
+			return 0;
+		}
+		char character;
+		int count = 0;
+		for (int i = 0; i < J.length(); i++) {
+			character = J.charAt(i);
+			for (int j = 0; j < S.length(); j++) {
+				if (S.charAt(j) == character) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+	
+    public int numJewelsInStonesHashing(String J, String S) {
+        if (J.length() == 0 || S.length() == 0) {
+			return 0;
+		}
+        Set<Character> set = new HashSet<Character>();
+        for(Character jewel : J.toCharArray()){
+            set.add(jewel);
+        }
+        int totalStoneCount = 0;
+        for(Character stone : S.toCharArray()){
+            if(set.contains(stone)){
+                ++totalStoneCount;
+            }
+        }
+        return totalStoneCount;
+    }
 }
